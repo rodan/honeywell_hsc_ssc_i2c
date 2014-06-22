4292317d6ae83aa8c03aded3ffa4ce35b3b9f65b
Petre Rodan <petre.rodan@simplex.ro>
2014-06-22 12:41:23 +0300

    add comments on function parameters

--- hsc_ssc_i2c.h
+++ hsc_ssc_i2c.h
@@ -13,7 +13,34 @@ struct cs_raw {
     uint16_t temperature_data;  // 11 bit
 };
 
+/// function that requests raw data from the sensor via i2c
+///
+/// input
+///  slave_addr    - i2c slave addr of the sensor chip
+///
+/// output
+///  raw           - struct containing 4 bytes of read data
+///
+/// returns
+///         0 if all is fine
+///         1 if chip is in command mode
+///         2 if old data is being read
+///         3 if a diagnostic fault is triggered in the chip
+///         4 if the sensor is not hooked up
 uint8_t ps_get_raw(const uint8_t slave_addr, struct cs_raw *raw);
+
+/// function that converts raw data read from the sensor into temperature and pressure values
+///
+/// input:
+///  raw            - struct containing all 4 bytes read from the sensor
+///  output_min     - output at minimal calibrated pressure (counts)
+///  output_max     - output at maximum calibrated pressure (counts)
+///  pressure_min   - minimal value of pressure range
+///  pressure_max   - maxium value of pressure range
+///
+/// output:
+///  pressure
+///  temperature
 uint8_t ps_convert(const struct cs_raw raw, float *pressure, float *temperature,
                    const uint16_t output_min, const uint16_t output_max, const float pressure_min,
                    const float pressure_max);
